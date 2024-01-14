189aefb9ca261859631befaccb2220d63bfd36ce
Jayden Sipe <jayden.sipe@gmail.com>
2024-01-13 19:05:52 -0500

    Added camera component and movement

--- addons/jaysreusablecomponentsandthings/components/fps-camera-component-3d.gd
+++ addons/jaysreusablecomponentsandthings/components/fps-camera-component-3d.gd
@@ -1,27 +1,178 @@
 extends Camera3D
 class_name FPSCameraComponent3D
 
-@export var actor: Node3D
-
 @export_group("FPS Settings")
+@export var character: CharacterBody3D
 @export var mouse_sensitivity: float = 0.005
-@export var rotation_amount: float = 2
-@export var rotation_return_time: float = 0.1
+
+# Camera controller settings and code used from: https://github.com/Btan2/Q_Move/
+enum BOB_TYPE { VB_COS, VB_SIN, VB_COS2, VB_SIN2 }
+
+@export_group("Bob Config")
+@export var enable_bob: bool = true
+@export var bob_mode: BOB_TYPE = BOB_TYPE.VB_SIN
+@export var bob_amount: float = 0.012            
+@export var bob_up: float = 0.5            
+@export var bob_cycle: float = 0.6         
+var _bob_times: Array = [0,0,0]
+var Q_bobtime: float = 0.0
+var Q_bob: float = 0.0
+
+@export_group("Idle Config")
+@export var enable_idle: bool = true
+@export var idle_scale: float = 1.6      
+var iyaw_cycle: float = 1.5       
+var iroll_cycle: float = 1.0     
+var ipitch_cycle: float = 2.0    
+var iyaw_level: float = 0.1       
+var iroll_level: float = 0.2         
+var ipitch_level: float = 0.15      
+var idleRight : float = 0.0
+var idleForward : float = 0.0
+var idleUp : float =  0.0
+
+var shakecam = false
+var shaketime : float = 0.0
+var shakelength = 0.0
+var deltaTime : float = 0.0
+var v_dmg_time : float = 0.0
+var v_dmg_roll : float = 0.0
+var v_dmg_pitch : float = 0.0
+
+@export_group("Roll Config")
+@export var enable_roll: bool = true
+@export var roll_amount: float = 15.0          
+@export var roll_speed: float = 300.0    
+	 
+const kick_time: float = 0.5         
+const kick_amount: float = 0.6       
+
+var idle_time: float = 0.0
+var mouse_move: Vector2 = Vector2.ZERO
+var mouse_rotation_x: float = 0.0
+var y_offset: float = 1.25          
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		actor.rotate_y(-event.relative.x * mouse_sensitivity)
-		rotate_x(-event.relative.y * mouse_sensitivity)
-		rotation.x = clamp(rotation.x, -PI/2, PI/2)
+		mouse_move = event.relative * 0.1
+		mouse_rotation_x -= event.relative.y * mouse_sensitivity
+		mouse_rotation_x = clamp(mouse_rotation_x, -90, 90)
+		character.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 
-func rotate_camera_left():
-	rotation.z = lerp_angle(rotation.z, deg_to_rad(rotation_amount), rotation_return_time)
-
-func rotate_camera_right():
-	rotation.z = lerp_angle(rotation.z, deg_to_rad(rotation_amount * -1), rotation_return_time)
+func _process(delta: float) -> void:
+	deltaTime = delta
 	
-func reset_rotation():
-	rotation.z = lerp_angle(rotation.z, deg_to_rad(0), rotation_return_time)
+	# Set points of origin
+	rotation_degrees = Vector3(mouse_rotation_x, 0, 0)
+	transform.origin = Vector3(0, y_offset, 0)
+	
+	# Apply velocity roll
+	if (enable_roll): velocity_roll()
+	
+	if character.velocity.length() <= 0.1:
+		_bob_times = [0,0,0]
+		Q_bobtime = 0.0
+		
+		if (enable_idle):
+			add_idle()
+			view_idle()
+	else:
+		idle_time = 0.0
+		
+		if (enable_bob): 
+			view_bob_classic()
+
+	# Apply damage/fall kicks
+	if v_dmg_time > 0.0:
+		rotation_degrees.z += v_dmg_time / kick_time * v_dmg_roll
+		rotation_degrees.x += v_dmg_time / kick_time * v_dmg_pitch
+		v_dmg_time -= delta
+	
+	# Apply shake
+	if shakecam:
+		shake(1)
+		
+func velocity_roll() -> void:
+	var side: float
+	
+	side = calc_roll(character.velocity, roll_amount, roll_speed) * 4
+	rotation_degrees.z += side
+
+func calc_roll(velocity: Vector3, angle: float, speed: float) -> float:
+	var s: float
+	var side: float
+	
+	side = velocity.dot(-get_global_transform().basis.x)
+	s = sign(side)
+	side = abs(side)
+	
+	if (side < speed):
+		side = side * angle / speed;
+	else:
+		side = angle;
+	
+	return side * s
+
+func add_idle():
+	idle_time += deltaTime
+	idleRight = idle_scale * sin(idle_time * ipitch_cycle) * ipitch_level
+	idleUp = idle_scale * sin(idle_time * iyaw_cycle) * iyaw_level
+	idleForward = idle_scale * sin(idle_time * iroll_cycle) * iroll_level
+
+func view_idle():
+	rotation_degrees.x += idleUp
+	rotation_degrees.y += idleRight
+	rotation_degrees.z += idleForward
+
+func view_bob_classic():
+	transform.origin[1] += calc_bob_classic()
+
+func calc_bob_classic():
+	var vel : Vector3
+	var cycle : float
+	
+	Q_bobtime += deltaTime
+	cycle = Q_bobtime - int(Q_bobtime / bob_cycle) * bob_cycle
+	cycle /= bob_cycle
+	if cycle < bob_up:
+		cycle = PI * cycle / bob_up
+	else:
+		cycle = PI + PI * (cycle - bob_up) / (1.0 - bob_up)
+	
+	vel = character.velocity
+	Q_bob = sqrt(vel[0] * vel[0] + vel[2] * vel[2]) * bob_amount
+	Q_bob = Q_bob * 0.3 + Q_bob * 0.7 * sin(cycle)
+	Q_bob = clamp(Q_bob, -7.0, 4.0)
+	
+	return Q_bob
+
+func trigger_shake(time: float):
+	shakecam = true
+	
+	shaketime = 0.0
+	shakelength = time
+	await get_tree().create_timer(time).timeout
+	
+	shakecam = false
+
+func shake(easing: int):
+	var cycle = Vector3(33, 44, 36)
+	var v_level = Vector3(-1.5, 2, 1.25)
+	var s_scale : float
+	
+	shaketime += deltaTime 
+	
+	easing = clamp(easing, 0, 2)
+	if easing == 0: # No shake easing
+		s_scale = 1.0
+	elif easing == 1: # Ease off scaling towards the end of the shake
+		var diff = shakelength - shaketime
+		s_scale = diff if diff <= 1.0 else 1.0
+	elif easing == 2: # Ease off scaling throughout the entire shake
+		s_scale = 1.0 - shaketime/shakelength
+	
+	for i in range(3):
+		rotation_degrees[i] += s_scale * sin(shaketime * cycle[i]) * v_level[i]
